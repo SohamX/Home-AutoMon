5d5042454808167207cbd6222c5fbf969115feab
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-11-13 22:35:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import './App.css';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import {useEffect,useState}  from 'react';
 
-function App() {
-  const [dat,setDat]=useState(0);
-  const [co,useCo]=useState(0);
-  const [smoke,useSmoke]=useState(0);
-     
-
-  
-  // useEffect(()=>
-  //   {
-  //     useCo(200);
-  //     if ((co>=65) || (smoke>=45) || (lpg>=550))
-  //     {
-  //       noti();
-  //     }
-  //   },[]);
+function App() {     
   return (
 
     <div className="App">
       <div className="container">
-        <Typography className="title" variant="h1">Home Automation and Monitoring System </Typography>
-        <iframe width="450" height="260" style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1898365/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&timescale=daily&type=line"></iframe>
-        <iframe width="450" height="260" style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1898365/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
-        <iframe width="450" height="260" style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1898365/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+      <h1 className='title'>
+        Home Automation and Monitoring System
+        </h1>
+        <div className='graph_div'>
+        <iframe style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1898365/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&timescale=daily&type=line"></iframe>
+        <iframe style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1898365/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+        <iframe style={{border: "1px solid #cccccc"}} src="https://thingspeak.com/channels/1898365/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+        </div>
       </div>
     </div>
   );
